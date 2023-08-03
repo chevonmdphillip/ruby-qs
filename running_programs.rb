1834685c12a67293739628cfd6a2f5cc30af3a8e
Chevon Phillip <chevonmdphillip@gmail.com>
2023-08-02 21:50:51 -0400

    Add string manipulation and conditional statement to running_programs.rb
    - Add code to convert a string to uppercase using the upcase method
    - Add code to replace all occurrences of 'l' with 's' in a string using the gsub method
    - Add code to check if a string includes the word 'Hello' using the include? method

--- running_programs.rb
+++ running_programs.rb
@@ -2,3 +2,11 @@
 
 name = 'Running Programs'
 puts "Starting #{name}..."
+
+statement = 'Hello I like this \'String\'!'
+puts statement.upcase
+
+new_statement = statement.gsub('l', 's')
+puts new_statement
+
+puts 'Statement includes the word Hello' if statement.include?('Hello')
